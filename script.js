57cc0a67e77b2418012e055cbd59406aa9184ccc
NurmuhammadvaSaidAmir <clash010a@gmail.com>
2025-04-24 08:02:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function light() {
+document.querySelector(".header")
+.style.backgroundColor = "white";
+document.querySelector(".pop")
+.style.color = "black";
+document.querySelector(".p")
+.style.color = "black";
+document.querySelector(".h4")
+.style.backgroundColor = "#FA8305"; 
+document.querySelector(".h4")
+.style.color = "black"; 
+document.body.style.backgroundColor = "white"; 
+document.body.style.color = "black"; 
+} function dark() { 
+document.querySelector(".header")
+.style.backgroundColor = "#121212"; 
+document.querySelector(".pop")
+.style.color = "white"; 
+document.querySelector(".p")
+.style.color = "#ccc"; 
+document.querySelector(".h4")
+.style.backgroundColor = "#FB982F"; 
+document.querySelector(".h4")
+.style.color = "white"; 
+document.body.style.backgroundColor = "#121212"; 
+document.body.style.color = "white"; 
+} 
